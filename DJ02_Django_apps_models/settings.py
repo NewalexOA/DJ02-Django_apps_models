068944e3e82054c1d662285a946a68532960572f
Anashkin Aleksei <109337222+NewalexOA@users.noreply.github.com>
2024-07-03 22:48:25 +0300

    Add templates and bootstrap styling
    ### [Описание на русском языке](#русский)
    
    ### [Description in English](#english)
    
    ---
    
    ## <a name="русский"></a>Описание на русском языке
    
    ### Описание
    
    Этот коммит представляет собой добавление шаблонов и стилизации с помощью Bootstrap. Включены следующие изменения:
    
    - Созданы отдельные файлы для меню и подвала сайта (`menu.html` и `footer.html`).
    - Подключение этих файлов к основному шаблону (`base.html`) через include.
    - Создание четырёх страниц сайта (`home.html`, `about.html`, `services.html`, `contact.html`) с использованием основного шаблона.
    - Стилизация всех страниц с помощью Bootstrap для обеспечения единого и современного вида сайта.
    
    Эти изменения помогают улучшить структуру кода, делая его более модульным и поддерживаемым. Использование Bootstrap улучшает пользовательский интерфейс и общее восприятие сайта.
    
    ## <a name="english"></a>Description in English
    
    ### Description
    
    This commit represents the addition of templates and styling using Bootstrap. The following changes are included:
    
    - Created separate files for the site menu and footer (`menu.html` and `footer.html`).
    - Connected these files to the main template (`base.html`) via include.
    - Created four site pages (`home.html`, `about.html`, `services.html`, `contact.html`) using the main template.
    - Styled all pages with Bootstrap to ensure a consistent and modern look for the site.
    
    These changes help improve the code structure, making it more modular and maintainable. The use of Bootstrap enhances the user interface and overall site appearance.

--- DJ02_Django_apps_models/settings.py
+++ DJ02_Django_apps_models/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'DJ02_Django_apps_models.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
